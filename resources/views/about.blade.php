796c5a9caf01eec35cd069a65888ad695c9becce
vladikslon <vladbrovi@gmail.com>
2025-03-22 00:04:07 +0000

    Created a view for'about us'

---
+++ resources/views/about.blade.php
@@ -0,0 +1,24 @@
+
+@extends('layouts.app')
+
+@section('content')
+    <div class="text-center p-15 bg-gray-800 text-white">
+        <h1 class="text-5xl font-bold">
+            About Us
+        </h1>
+    </div>
+
+    <div class="m-auto w-4/5 py-15">
+        <div class="text-gray-700 text-lg">
+            <p class="py-6">
+                Welcome to our company! We are dedicated to providing the best service possible.
+            </p>
+            <p class="py-6">
+                Our team is composed of experienced professionals who are passionate about what they do.
+            </p>
+            <p class="py-6">
+                We believe in innovation, integrity, and excellence in all our endeavors.
+            </p>
+        </div>
+    </div>
+@endsection
